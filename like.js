0876735a9bcf6e6135ebd4cef6e743214be98189
Unknown <jdeansart@gmail.com>
2017-10-04 13:21:10 -0500

    jquery

--- like.js
+++ like.js
@@ -1,9 +1,22 @@
-var btns = Array.from(document.getElementsByClassName("increment"));
+// var btns = Array.from(document.getElementsByClassName("increment"));
 
-btns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        let currentCount = btn.nextSibling.textContent.trim()
-        newCount = parseInt(currentCount) + 1
-        btn.nextSibling.textContent = newCount;
+// btns.forEach(btn => {
+//     btn.addEventListener('click', e => {
+//         let currentCount = btn.nextSibling.textContent.trim()
+//         newCount = parseInt(currentCount) + 1
+//         btn.nextSibling.textContent = newCount;
+//     })
+// })
+$(function(){
+    $('.increment').click(event => {
+        console.log(event)
+        let currentCount = $(event.target).next().text()
+        console.log(currentCount)
+        return currentCount*1+1
+        
     })
-})
+});
+
+// $('.increment').click(function() {
+//     $('.increment').html(function(i, val) { return val*1+1 });
+// });
