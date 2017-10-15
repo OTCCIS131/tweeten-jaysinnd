942f062e0cd50cd889051b70faca0f5053fd51b0
Unknown <jdeansart@gmail.com>
2017-10-15 01:38:36 -0500

    Working.. except for the like and refresh buttons on new post

--- like.js
+++ like.js
@@ -5,6 +5,8 @@
 //         btn.nextSibling.textContent = newCount;
 //     })
 // })
+
+//Add 'likes' to the count.
 $(function(){
     $('.increment').click(event => {
         let currentCount = $(event.target).next().text()
@@ -14,18 +16,37 @@ $(function(){
     })
 });
 
-$(function(){
-    $('#newtweet').submit(function (e) {
-        e.preventDefault()
-        let newTweet = $("[name=title]", $(this))
-        let newTweeten = newTweet.val()
+//Re-tweet, and new tweet 
+// $('.tweetsub').click(function(event) {
+//         event.preventDefault()
+//         let newTweet = $(".newest").val()
+//         if (newTweet.trim() == '') return
+//             let num = $('.clonedForm').length
+//             let newNum = new Number(num + 1)
+//             let newElem = $('#retweet' + num).clone().attr('id', 'retweet' + newNum)
+//             //let newElement = $('#tweettext').replaceWith(newTweet)
+//         $('#retweet' + num).prepend(newElem)
+//         $('#tweettext').replaceWith(newTweet)
+  
+// $(".newest").val("")
 
-        if (newTweet.trim() == '') return
+$('.tweetsub').click(function(event){
+    event.preventDefault()
+    let newTweet = $(".newest").val()
+    if (newTweet.trim() == '') return;
+    let numOne = 0;
+    let numTwo = 0;
 
-        let newElement = $(":last-child", $("#tweets")).clone()
-        newElement.text(newTweet)
-        
+    
 
-    })
+    $('.subtweets').prepend("<div class=\"panel panel-default\">" +
+                            "<div class=\"panel-body\">" +
+                                "<div><img src=\"miken.jpg\" alt=\"miken\" class=\"img-circle\" width= 50px height= 50px>" +
+                                "<h3><b>Jason Dean</b></h3>" +
+                                "<p>" + newTweet + "</p>" +
+                                "<div><i class=\"glyphicon glyphicon-heart increment\"></i><span>" + numOne + "</span>" +
+                                "<span class=\"glyphicon glyphicon-refresh increment\"></span><span>" + numTwo + "</span>" +
+                                "</div></div>");
+
+    $(".newest").val("")
 })
-
